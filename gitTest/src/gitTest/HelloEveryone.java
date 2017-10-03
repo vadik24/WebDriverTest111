ce0b692890fc7b51bf7f2da0729ae812a3ccf0a3
Vadim Belonenko <v_belonenko@hotmail.com>
2017-10-02 22:12:41 -0700

    update 2

--- gitTest/src/gitTest/HelloEveryone.java
+++ gitTest/src/gitTest/HelloEveryone.java
@@ -6,7 +6,7 @@ public class HelloEveryone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HWEHR");
+		System.out.println("HWEHRhiiii");
 	}
 
 }
